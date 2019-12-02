e5435d0d56859cd33aaf8bbdb77076912935fdaa
Lduffley <53620669+Lduffley@users.noreply.github.com>
2019-12-02 14:26:13 -0600

    Changed Import Name of CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import "./index.css";
+import "./styles.css";
 
 let rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
